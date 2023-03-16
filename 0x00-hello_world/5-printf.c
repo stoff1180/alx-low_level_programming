d00935e091a0e06119c6a1b43dc876ab330f3dba
stoff1180 <stoffmustapha1180@gmail.com>
2023-03-16 11:20:49 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with printf function
+ * return 0 (success)
+ */
+int main (void)
+{
+        printf ("\"Programming is like building a multilingual puzzle\n");
+        return (0);
+}
