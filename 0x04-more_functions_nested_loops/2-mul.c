2e5bd63da2d3ce6af52279aa67ad941eecff3c02
stoff1180 <stoffmustapha1180@gmail.com>
2023-03-23 07:46:54 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ * @a: parameter
+ * @b: parameter
+ * Return: alwyas multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
