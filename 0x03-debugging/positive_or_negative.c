17235086a6eabd12b7a1f76b079ada49767047c7
stoff1180 <stoffmustapha1180@gmail.com>
2023-03-24 16:36:33 -0700

    update

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,10 +6,16 @@
  */
 void positive_or_negative(int i)
 {
-	if (i < 0)
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
 	{
 		printf("%d is negative\n", i);
 	}
-	else
-		printf("%d is positive", i);
 }
