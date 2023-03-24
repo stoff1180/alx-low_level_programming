812c83dd3dd542a993a982b40de15c1b65543dbe
stoff1180 <stoffmustapha1180@gmail.com>
2023-03-24 05:23:01 -0700

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * main - test function for positive or negative
- * Return: always 0
- */
+* main - Test function for positive or negative
+* Return: 0
+*/
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
