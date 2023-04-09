27524b82557579c2b8d9c7791dc279330a714e47
stoff1180 <stoffmustapha1180@gmail.com>
2023-04-08 17:37:20 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - that prints its name, followed by a new line
+ * @argc: parameter int
+ * @argv: paraameter list
+ * Return: always 0
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
