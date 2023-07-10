3e095ce1befdb9635bae8858ef658ddbd7d11370
stoff1180 <stoffmustapha1180@gmail.com>
2023-07-10 16:30:19 -0700

    file I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,19 +8,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
+	int fd;
 	ssize_t bytes;
-	ssize_t t;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, &buf[0], letters);
-	bytes = write(STDOUT_FILENO,&buf[0], t);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 
-	free(buf);
 	close(fd);
 	return (bytes);
 }
